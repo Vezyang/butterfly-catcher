9f38b5e512162cb4b7e0eab89eb7e89e6369b443
Vezyang <vezyang@gmail.com>
2022-03-21 20:49:42 +0500

    New butterfly spawn system

--- UnityGame/Assets/Scripts/GameManager.cs
+++ UnityGame/Assets/Scripts/GameManager.cs
@@ -15,15 +15,18 @@ public class GameManager : MonoBehaviour
     [Header("Spawn settings:")]
     public Transform[] spawnPoints = new Transform[1];
 
+    private float spawnRate = 3f;
+    private float nextSpawn;
+
     void Start()
     {
         Vector3 tLEdge = Camera.main.ViewportToWorldPoint(new Vector3(0f, 1f, Camera.main.nearClipPlane));
         Vector3 rBEdge = Camera.main.ViewportToWorldPoint(new Vector3(1f, 0f, Camera.main.nearClipPlane));
 
-        spawnPoints[0].position = new Vector3(tLEdge.x, 1, tLEdge.z);
-        spawnPoints[1].position = new Vector3(rBEdge.x, 1, tLEdge.z);
-        spawnPoints[2].position = new Vector3(tLEdge.x, 1, rBEdge.z);
-        spawnPoints[3].position = new Vector3(rBEdge.x, 1, rBEdge.z);
+        spawnPoints[0].position = new Vector3(tLEdge.x - 0.5f, 1, tLEdge.z + 0.5f);
+        spawnPoints[1].position = new Vector3(rBEdge.x + 0.5f, 1, tLEdge.z + 0.5f);
+        spawnPoints[2].position = new Vector3(tLEdge.x - 0.5f, 1, rBEdge.z - 0.5f);
+        spawnPoints[3].position = new Vector3(rBEdge.x + 0.5f, 1, rBEdge.z - 0.5f);
 
         moveButterfliesX = tLEdge.x;
         moveButterfliesZ = rBEdge.z;
@@ -33,12 +36,15 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         GameObject[] butterflies = GameObject.FindGameObjectsWithTag("Butterfly");
-
-
-        if (butterflies.Length <= 0)
+        if (butterflies.Length <= 2f)
         {
             SpawnButterfly(Random.Range(1, 4), butterflyPrefabs[Random.Range(0, butterflyPrefabs.Length)]);
         }
+        else if(Time.time > nextSpawn && butterflies.Length < 5f)
+        {
+            RandomSpawnButterfly(3);
+        }
+
     }
 
     public void SpawnButterfly(int number, GameObject butterfly)
@@ -58,6 +64,13 @@ public class GameManager : MonoBehaviour
             }
         }
         Debug.Log($"Spawned: {number} \"{butterfly.name}\"");
+    }
+
+    private void RandomSpawnButterfly(int max)
+    {
+        spawnRate = Random.Range(3, 5);
+        nextSpawn = Time.time + spawnRate;
+        SpawnButterfly(Random.Range(1, max), butterflyPrefabs[Random.Range(0, butterflyPrefabs.Length)]);
     }
 
     public void AddToScore(int num)
